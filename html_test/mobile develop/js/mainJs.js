b3b0b49edfac5e24d5bec6cc1fbdc4d16baf1e0b
ashuo <478830798@qq.com>
2017-12-06 15:26:37 +0800

    Signed-off-by: ashuo <478830798@qq.com> bootstrap

--- html_test/mobile develop/js/mainJs.js
+++ html_test/mobile develop/js/mainJs.js
@@ -1,11 +1,14 @@
 $(function(){
     setbgImage();
     $(window).on("resize",function(){
-        setbgImage();
-    });
-    function setbgImage(){
-        //获取屏幕宽度
         var screenWidth = $(window).width();
+        setbgImage(screenWidth);
+        setTab();
+
+    });
+    function setbgImage(screenWidth){
+        //获取屏幕宽度
+
 //    判断屏幕类型
         var isSmallScreen = screenWidth < 768 ;
         $("#main_ad > .carousel-inner > .item").each(function(index ,value){
@@ -22,4 +25,41 @@ $(function(){
             }
         });
     }
+    $('[data-toggle="tooltip"]').tooltip();
+//    设置tab栏的ul外边框宽度
+    function setTab(){
+        var liNumWidth = 30;
+        $(".nav-tabs li").each(function(index,element){
+            liNumWidth += element.clientWidth;
+        });
+        if(liNumWidth > $(window).width()){
+            $(".nav-tabs").css("width",liNumWidth).parent().css("overflow-x","scroll");
+
+        }
+    }
+
+//给新闻导航栏设置点击事件
+    $(".main-new-two li").on("click",function(){
+        $(".main-new-one").text($(this).data("description"));
+    });
+
+    //轮播图向左向右滑动
+
+
+var startX,endX;
+    $(".carousel").on("touchstart",function(e){
+        startX = e.originalEvent.touches[0].clientX;
+    });
+    $(".carousel").on("touchmove",function(e){
+        endX = e.originalEvent.touches[0].clientX;
+    });
+    $(".carousel").on("touchend",function(e){
+        if(Math.abs(startX-endX) > 40){
+            if(startX > endX ){
+                $("#main_ad").carousel("next");
+            }else{
+                $("#main_ad").carousel("prev");
+            }
+        }
+    });
 });
