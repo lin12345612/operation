fd05f8b83fb513615a7803e18b988be3dd1931e0
lin12345612 <zxc123>
2018-01-12 17:47:56 +0800

    Signed-off-by: lin12345612 <zxc123.> reactjs练习

--- html_test_one/reactjs/app.js
+++ html_test_one/reactjs/app.js
@@ -1,11 +1,1 @@
-class WorldMessage extends React.Component{
-    render(){
-        return (
-            <div>
-            Hello!{this.props.name}
-    </div>
-    )
-    }
-}
-var container = document.getElementById('container');
-ReactDOM.render(<WorldMessage name="world"/>, container);
+
--- html_test_one/reactjs/src/js/my-compiled.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-//# sourceMappingURL=my-compiled.js.map
